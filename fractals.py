322eb29bfbbeada1ec1ffb786c23019699501af9
Expedito Hebert <shinigamecontato@gmail.com>
2024-05-26 16:23:51 -0300

    Sierpinski Triangle

--- fractals.py
+++ fractals.py
@@ -10,3 +10,15 @@ def draw_fractal_honeycomb(pencil: turtle.Turtle, l_system: LSystem, generations
         elif command == 'B':
             pencil.right(l_system.angle)
             pencil.forward(20)
+            
+def draw_fractal_sierpinski_triangle(pencil: turtle.Turtle, l_system: LSystem, generations: int):
+    instructions = l_system.generate(generations)
+    for command in instructions:
+        if command == 'F':
+            pencil.forward(20)
+        elif command == 'G':
+            pencil.forward(20)  
+        elif command == '+':
+            pencil.left(l_system.angle)
+        elif command == '-':
+            pencil.right(l_system.angle)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import turtle
 import time
 from lsystem import LSystem
 from funcs import setup_window, setup_pencil
-from fractals import draw_fractal_honeycomb
+from fractals import draw_fractal_honeycomb, draw_fractal_sierpinski_triangle
 
 # Configurações da tela e do lápis
 screen = turtle.Screen()
@@ -12,8 +12,9 @@ pencil = turtle.Turtle()
 setup_pencil(pencil, 3, 1, 'orange')
 
 
-# Criação do LSystem e geração do resultado
-l_system = LSystem("A", {"A": "AB","B": "A",}, 60)
+# Criação do LSystem
+#l_system_1 = LSystem("A", {"A": "AB","B": "A",}, 60)
+l_system_2 = LSystem("F", {"F": "F-G+F+G-F","G": "GG",}, 120)
 
 # Desenho do fractal
 for gen in range(1, 21):  # Gerações de 1 a 20
@@ -22,8 +23,9 @@ for gen in range(1, 21):  # Gerações de 1 a 20
     setup_pencil(pencil, 3, 1, 'orange')
     screen.title(f"Geração: {gen}")
     
-    # Tipo de Fractal
-    draw_fractal_honeycomb(pencil, l_system, gen)
+    # Tipo de fractal
+    #draw_fractal_honeycomb(pencil, l_system_1, gen)
+    draw_fractal_sierpinski_triangle(pencil, l_system_2, gen)
     
     # Pausa para ver a geração
     time.sleep(3)
